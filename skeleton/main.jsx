23bcbf11583c9448370d8749b5e2acc8bc427025
Nate Reiners <nate.reiners@gmail.com>
2016-12-23 16:01:46 -0800

    Add tab widget

--- skeleton/main.jsx
+++ skeleton/main.jsx
@@ -1,4 +1,18 @@
-<h1>'HI'</h1>
+import Tabs from './tabs';
+import React from 'react';
+
+const tabs = [{title: 'one', content: 'I am the first'}, {title: 'two', content: 'Be creative Nate'}, {title: 'three', content: 'react is cool!' }];
+
+const Main = () => (
+  <div className="main">
+    <div className="tabs-div">
+      <Tabs tabList={tabs} />
+    </div>
+
+    <div className="autocomplete-div">
+    </div>
+  </div>
+);
 
 
-exports default Main
+export default Main;
---
+++ skeleton/tabs.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {title: "", content: "", index: 0 };
+  }
+
+  selectTab() {
+  }
+
+
+  render() {
+    // debugger;
+    let tabs = this.props.tabList.map((tab, index) => (
+      <li key={index}
+        className = {(this.state.title === tab.title) ? "bold" : ""}
+        onClick={() => (this.setState({ title: tab.title, content: tab.content, index }))}>
+        {tab.title}
+      </li>
+    ));
+
+    return (
+      <div className="tabscontainer">
+        <div className="tabsHeader">
+          <ul>
+            {tabs}
+          </ul>
+        </div>
+
+        <div className="tabsContent">
+          <h1>{this.state.content}</h1>
+        </div>
+      </div>
+
+
+    );
+  }
+}
+
+export default Tabs;
